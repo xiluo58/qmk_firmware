e81f2117c1d53738dcf2a396821a961d29c73630
Xi Luo <luoxi58@gmail.com>
2018-10-07 22:43:35 +0800

    Download source generated by Ergodox Ez. Made folloing change: 1. Fix make command doesn't work. 2. Enable bootmagic

---
+++ keyboards/ergodox_ez/keymaps/xl/rules.mk
@@ -0,0 +1,4 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+
+BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration(+1000)
