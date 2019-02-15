a19afe359f17dacad243601a2f3625a49dd0f23b
xiluo58 <luoxi58@gmail.com>
2019-02-15 10:57:54 +0800

    Update rules.mk
    yes is deprecated. Use full

--- keyboards/ergodox_ez/keymaps/xl/rules.mk
+++ keyboards/ergodox_ez/keymaps/xl/rules.mk
@@ -1,7 +1,7 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 
-BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = full  # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE  = no # Mouse keys(+4700)
 RGBLIGHT_ENABLE = no
 LEADER_ENABLE = yes
